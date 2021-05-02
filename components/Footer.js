d5c3d16af85780c61f85585a6b3cb4bbe2dbe9dc
ieadoboe <ikeadoboe1@gmail.com>
2021-05-02 00:39:47 +0000

    footer 1.0 linear gradient

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,61 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <footer className="container">
-      Copyright 2021 Envision Digital
+    <footer className="footer-section">
+      <section className="footer container">
+        <div>
+          <ul className="list-reset sitemap">
+            <li>
+              <h4>Sitemap</h4>
+            </li>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact">
+                <a>Contact</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/faq">
+                <a>FAQs</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>Privacy Policy</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="company-info">
+          <h4>ENVISION DIGITAL</h4>
+          <div>
+            <p>4545 East McKinley Street</p>
+            <p>Accra, Greater Accra 85008</p>
+          </div>
+          <div>
+            <p>0 (233) 500 000 000</p>
+            <p>info@envisiondigitals.com</p>
+          </div>
+
+          <div>
+            <p>
+              <span>&copy;</span> Copyright 2021 Envision Digital
+            </p>
+          </div>
+        </div>
+      </section>
     </footer>
   );
-}
- 
+};
+
 export default Footer;
