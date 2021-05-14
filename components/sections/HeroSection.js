2b69f0725c754d42453b1add7b60f0c3c1019639
ieadoboe <ikeadoboe1@gmail.com>
2021-05-14 13:57:40 +0000

    typing effect in herosection done

--- components/sections/HeroSection.js
+++ components/sections/HeroSection.js
@@ -2,22 +2,35 @@ import Link from "next/link";
 import Button from "./../elements/Button";
 import Navbar from "../../components/layout/Navbar";
 import styles from "./../../styles/HeroSection.module.scss";
+import Typical from "react-typical";
 
 const HeroSection = () => {
-    return (
-        <>
-        <Navbar />
-        <div className={`container ${styles["hero-section-inner"]}`}>
-          <h1>Reach your most important customers with ease.</h1>
-          <p>We help businesses reach a more targeted audience.</p>
-          <div>
-            <Link href="/contact">
-              <Button long={true}>Contact</Button>
-            </Link>
-          </div>
+  return (
+    <>
+      <Navbar />
+      <div className={`container ${styles["hero-section-inner"]}`}>
+            <h1 className>Reach your most important{" "}
+            <Typical 
+              loop={Infinity}
+              wrapper="span"
+              steps={[
+                "customers",
+                10000,
+                "audiences",
+                10000,
+                "clients",
+                10000
+              ]}
+            /></h1>
+        <p>We help businesses reach a more targeted audience.</p>
+        <div>
+          <Link href="/contact">
+            <Button long={true}>Contact</Button>
+          </Link>
         </div>
-        </>
-    );
-}
- 
+      </div>
+    </>
+  );
+};
+
 export default HeroSection;
